851cc6960f9ca1e64add8b9c0834e00db119f7ee
Hugo <blanchardhugo.d@gmail.com>
2020-11-04 12:27:05 +0100

    Update solution.js
    Added the functionality of choosing the content of the pyramid

--- solution.js
+++ solution.js
@@ -1,19 +1,22 @@
-function pyramid(n) {
+function pyramid(n, content) {
   if (n < 1) {
     return [];
   } else {
     const answer = pyramid(n - 1);
-    answer.push(storey(n));
+    answer.push(storey(n, content));
     return answer;
   }
 }
 
-function storey(n) {
+function storey(n, content) {
   if (n < 1) {
     return [];
   } else {
     const answer = storey(n - 1);
-    answer.push(1);
+    answer.push(content);
     return answer;
   }
 }
+
+//the console.log(pyramid(4, 'brick')); would return :
+//[['brick'], ['brick', 'brick'], ['brick', 'brick', 'brick'], ['brick', 'brick', 'brick', 'brick']]
